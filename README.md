6df3b02e93cc3898c772c6749610ce87005feb8e
Las45 <matteo.morent@gmail.com>
2026-09-15 20:52:27 +0200

    Finished it
    Just some style has to be added

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Its simultaion cells like in a video.
 
 The *rules*: A cell only *lives* if it has *2 or 3 neighbours*. Else it will *die*.
+The whole project, except the last commit, is done with no AI. At the last commit were some logic errors that AI marked, but they were solved by me. So *no line of code is from AI*.
--- classes/cell.py
+++ classes/cell.py
@@ -32,22 +32,24 @@ class Cell:
                 if j+1 < len(self.field[i]):
                     if self.field[i][j+1] == 1:
                         self.neightbour+=1
-                if self.neightbour < 2 or self.neightbour > 3:
-                    self.to_update.append((i, j))
-                #else:
-                #    self.to_update2.append((i, j))
+                if i-1 >= 0 and j-1 >= 0:
+                    if self.field[i-1][j-1] == 1:
+                        self.neightbour+=1
+                if i-1 >= 0 and j+1 < len(self.field[i]):
+                    if self.field[i-1][j+1] == 1:
+                        self.neightbour+=1
+                if i+1 < len(self.field) and j-1 >= 0:
+                    if self.field[i+1][j-1] == 1:
+                        self.neightbour+=1
+                if i+1 < len(self.field) and j+1 < len(self.field[i]):
+                    if self.field[i+1][j+1] == 1:
+                        self.neightbour+=1
+                if (self.neightbour < 2 or self.neightbour > 3) and self.field[i][j] == 1:
+                    self.to_update.append((i, j, 0))
+                elif self.neightbour == 3 and self.field[i][j] == 0:
+                    self.to_update.append((i, j, 1))
                 self.neightbour = 0
 
-        for i, j in self.to_update:
-            if self.field[i][j] == 1:
-                self.field[i][j] = 0
-            else:
-                self.field[i][j] = 1
+        for i, j, value in self.to_update:
+            self.field[i][j] = value
         self.to_update.clear()
-
-        #for i, j in self.to_update2:
-        #    if self.field[i][j] == 1:
-        #        self.field[i][j] = 0
-        #    else:
-        #        self.field[i][j] = 1
-        #self.to_update2.clear()
